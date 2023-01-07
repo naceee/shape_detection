d80ca87a6ba71ed1aed6c8997810e55924ed01b5
naceee <47631585+naceee@users.noreply.github.com>
2023-01-07 22:37:13 +0100

    added functions for sampling more objects

--- sampling_points.py
+++ sampling_points.py
@@ -7,6 +7,24 @@ import warnings
 import os
 
 
+def plane_equation(p1, p2, p3):
+    x1, y1, z1 = p1
+    x2, y2, z2 = p2
+    x3, y3, z3 = p3
+
+    a1 = x2 - x1
+    b1 = y2 - y1
+    c1 = z2 - z1
+    a2 = x3 - x1
+    b2 = y3 - y1
+    c2 = z3 - z1
+    a = b1 * c2 - b2 * c1
+    b = a2 * c1 - a1 * c2
+    c = a1 * b2 - b1 * a2
+    d = (- a * x1 - b * y1 - c * z1)
+    return a, b, c, d
+
+
 def plot_point_cloud(points, title="", figsize=(6, 6), num_points=500):
     # sorry this is very ugly
     warnings.filterwarnings("ignore")
@@ -144,6 +162,67 @@ def point_cloud(shape, n, sigma, params=None):
                 z = random.uniform(-c, c)
             points[i, :] = [x, y, z]
 
+    elif shape == "tetrahedron":
+        e = params["edge_len"]
+        p = [(e, 0, 0), (0, e, 0), (0, 0, e), (e, e, e)]
+        planes = [plane_equation(p[0], p[1], p[2]),
+                  plane_equation(p[0], p[1], p[3]),
+                  plane_equation(p[0], p[2], p[3]),
+                  plane_equation(p[1], p[2], p[3])]
+
+        for i in range(n):
+            side = random.randint(0, 3)
+
+            x = random.uniform(0, e)
+            y = random.uniform(0, e)
+            z = (- planes[side][0] * x - planes[side][1] * y - planes[side][3]) / planes[side][2]
+
+            while not 0 <= z <= e:
+                x = random.uniform(0, e)
+                y = random.uniform(0, e)
+                z = (- planes[side][0] * x - planes[side][1] * y - planes[side][3])/planes[side][2]
+
+            points[i, :] = [x, y, z]
+
+    elif shape == "banana":
+        phi_max = params["phi"]
+        r_max = params["r"]
+        R = params["R"]
+
+        for i in range(n):
+            phi = random.uniform(0, phi_max)
+            r = np.power(np.sin(phi * np.pi / phi_max), 0.4) * r_max
+            theta = random.uniform(0, 2 * np.pi)
+
+            x = (R + r * math.cos(theta)) * math.cos(phi) + random.gauss(0, sigma)
+            y = (R + r * math.cos(theta)) * math.sin(phi) + random.gauss(0, sigma)
+            z = r * math.sin(theta) + random.gauss(0, sigma)
+
+            points[i, :] = [x, y, z]
+
+    elif shape == "filled_cube":
+        edge_len = params["edge_len"]
+        for i in range(n):
+            x = random.uniform(-edge_len, edge_len)
+            y = random.uniform(-edge_len, edge_len)
+            z = random.uniform(-edge_len, edge_len)
+
+            points[i, :] = [x, y, z]
+
+    elif shape == "filled_sphere":
+        r = params["r"]
+        for i in range(n):
+            x = random.uniform(-r, r)
+            y = random.uniform(-r, r)
+            z = random.uniform(-r, r)
+
+            while x**2 + y**2 + z**2 > r:
+                x = random.uniform(-r, r)
+                y = random.uniform(-r, r)
+                z = random.uniform(-r, r)
+
+            points[i, :] = [x, y, z]
+
     else:
         raise Exception("invalid shape")
 
@@ -206,10 +285,13 @@ def plot_shapes(shape=None, n=0):
 
 
 def main():
-    for i in range(0, 40):
+    """
+    for i in range(0, 100):
         a = random.uniform(1, 1.5)
         b = random.uniform(0.5, 1)
         c = random.uniform(0.2, 0.5)
+        phi = random.uniform(math.pi / 4, math.pi)
+
         shape_dicts = {
             "sphere": {"r": 1},
             "torus": {"r": c, "R": a},
@@ -217,11 +299,17 @@ def main():
             "line": {"direction": (1, 1, 0.2), "length": 1},
             "cylinder": {"r": c, "height": a},
             "cuboid": {"a": a, "b": b, "c": c},
-            "ellipsoid": {"a": a, "b": b, "c": c}
+            "ellipsoid": {"a": a, "b": b, "c": c},
+            "tetrahedron": {"edge_len": 1},
+            "banana": {"phi": phi, "r": c, "R": a},
+            "filled_sphere": {"r": 1},
+            "filled_cube": {"edge_len": 1}
         }
-        create_rotated_point_clouds(shape_dicts, 500, 0.01, i, i+1)
 
-    plot_shapes()
+        create_rotated_point_clouds(shape_dicts, 500, 0.01, i, i+1)
+    """
+
+    plot_shapes(shape="filled_cube", n=22)
 
 
 if __name__ == "__main__":
