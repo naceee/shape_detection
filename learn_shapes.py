19e4ecde99cfbe43be863475e77bfbe6faf48c83
naceee <47631585+naceee@users.noreply.github.com>
2023-01-15 13:12:49 +0100

    predicitng shapes with cross validation

--- learn_shapes.py
+++ learn_shapes.py
@@ -1,43 +1,75 @@
 import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_predict
 import pandas as pd
-from sklearn import metrics
+import warnings
+from computing_persistent_homology import get_persistence_from_df
+from persistent_images import create_persistence_image
 
 
-def train_model(resolution=4):
-    data = pd.read_csv(f"persistence_images_data/data_n=100_resolution={resolution}.csv")
+def train_model(resolution):
+    warnings.filterwarnings("ignore")
+
+    data = pd.read_csv(f"persistence_images_data/data_n=100_resolution={resolution}.csv",
+                       index_col="Unnamed: 0")
     X = data.loc[:, data.columns != 'result']
     y = data["result"]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
-    # Create a Gaussian Classifier
-    clf = RandomForestClassifier(bootstrap=True, criterion='gini', max_depth=10, n_estimators=1000)
+    clf = RandomForestClassifier(bootstrap=True, criterion="gini", max_depth=16,
+                                 n_estimators=4000)
+    y_pred = cross_val_predict(clf, X, y, cv=5)
 
-    # Train the model using the training sets
-    clf.fit(X_train, y_train)
+    classes = set(y)
+    d = {c: i for i, c in enumerate(classes)}
+    predictions = np.zeros((len(classes), len(classes)))
+    for actual, predicted in zip(y, y_pred):
+        predictions[d[actual], d[predicted]] += 1
 
-    y_pred = clf.predict(X_test)
+    acc = np.trace(predictions) / np.sum(predictions)
 
-    print("Accuracy on training data:", metrics.accuracy_score(y_train, clf.predict(X_train)))
-    print("Accuracy on test data:", metrics.accuracy_score(y_test, y_pred))
+    print(f"resolution: {resolution}, acc: {acc}")
 
-    print("wrong predictions:")
-    print("value     | predicted")
-    print("---------------------")
-    for (t, p) in zip(y_test, y_pred):
-        if t != p:
-            print(f"{t:9} | {p:9}")
+    plt.rcParams["figure.figsize"] = (7, 7)
+    plt.matshow(predictions)
+    plt.xticks(list(range(len(d.keys()))), d.keys(), rotation=45)
+    plt.yticks(list(range(len(d.keys()))), d.keys(), rotation=0)
+    plt.savefig(f"images/acc_r={r}.pdf")
+    plt.show()
 
-    return metrics.accuracy_score(y_test, y_pred)
+    return acc
+
+
+def predicting_cyclists(resolution=4):
+    data = pd.read_csv(f"persistence_images_data/4D_data_n=100_resolution={resolution}.csv",
+                       index_col="Unnamed: 0")
+    data = data.dropna()
+    X = data.loc[:, data.columns != 'result']
+    y = data["result"]
+
+    cyclists_df = pd.read_csv("cyclists.csv", index_col="Unnamed: 0")
+    teams = set(cyclists_df["team"])
+
+    clf = RandomForestClassifier(bootstrap=True, criterion="gini", max_depth=20,
+                                 n_estimators=1000)
+    clf.fit(X, y)
+
+    print(f"{clf.classes_}")
+    for team in teams:
+        team_df = cyclists_df.loc[cyclists_df['team'] == team]
+        team_df = team_df[["MOUNTAIN", "TIMETRAIL", "SPRINT", "COBBLE"]]
+
+        team_persistence = get_persistence_from_df(team_df, max_dimensions=4)
+        team_persistence.to_csv(f"cyclists_persistences/{team}.csv")
+
+        persistence_image = create_persistence_image(f"cyclists_persistences/{team}.csv",
+                                                     plot=False, resolution=resolution)
+
+        print(team, clf.predict_proba(persistence_image.reshape(1, -1))[0])
 
 
 if __name__ == "__main__":
-    y = []
-    for r in range(2, 8):
-        print("resolution:", r)
-        y.append(train_model(r))
-        print()
-
-    plt.plot(list(range(2, 8)), y)
-    plt.show()
+    # predicting_cyclists(resolution=4)
+    all_scores = []
+    for r in range(2, 10):
+        acc = train_model(r)
